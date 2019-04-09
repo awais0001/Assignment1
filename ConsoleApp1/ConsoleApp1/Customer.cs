e5337c02cd7469d97ceab98822ab4ab428febab1
awais0001 <awaissahi987@gmail.com>
2019-04-09 18:30:34 +0500

    Committing All Files
    Signed-off-by: SyedZeeshanYousaf <zeeyousaf100@gmail.com>

---
+++ ConsoleApp1/ConsoleApp1/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleApp1
+{
+    internal class Customer
+    {
+        public static string First { get; private set; }
+        public static string Last { get; private set; }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static List<Customer> customers = new List<Customer>
+        {
+            new Customer = { First = "Tabii", Last = "baig" },
+            new Customer = { First = "Murad", Last = "Ahmad" }
+        };
+
+        
+
+        static void Main(string[] args)
+        {
+            IEnumerable<Customer> Q =
+                from Cust in customers
+                select Cust;
+            foreach (Customer c in query)
+            {
+                
+            }
+        }
+    }
+}
